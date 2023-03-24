edd4dfa0c597940931feb47548bc0c2ee75ca241
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-24 05:17:35 -0500

    Add definition search from dictionary

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
 # curl https://www.example.com/
 
-curl ftp://ftp.funet.fi/ http://www.weirdserver.com:8000/
+# curl ftp://ftp.funet.fi/ http://www.weirdserver.com:8000/
+
+curl dict://dict.org/m:curl
