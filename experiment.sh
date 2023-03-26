bc97c8ae77f1ea02cbb6a55677cc6761858ee784
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-26 12:13:12 -0500

    Store result into json file

--- experiment.sh
+++ experiment.sh
@@ -3,4 +3,4 @@ source .env
 curl --request GET \
 	--url 'https://mlb-data.p.rapidapi.com/json/named.team_all_season.bam?season='\''2017'\''&all_star_sw='\''N'\''&sort_order=name_asc' \
 	--header 'X-RapidAPI-Host: mlb-data.p.rapidapi.com' \
-	--header 'X-RapidAPI-Key: '$KEY
+	--header 'X-RapidAPI-Key: '$KEY > result.json
