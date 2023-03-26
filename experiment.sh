c787f8a25e8682791c599201501986057b2914c2
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-26 12:23:58 -0500

    Add loop version of cURL command

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source .env
+YEAR=2017
+while [ $YEAR -le 2019 ]
+do
 curl --request GET \
-	--url 'https://mlb-data.p.rapidapi.com/json/named.team_all_season.bam?season='\''2017'\''&all_star_sw='\''N'\''&sort_order=name_asc' \
+	--url 'https://mlb-data.p.rapidapi.com/json/named.team_all_season.bam?season='\'$YEAR\''&all_star_sw='\''N'\''&sort_order=name_asc' \
 	--header 'X-RapidAPI-Host: mlb-data.p.rapidapi.com' \
-	--header 'X-RapidAPI-Key: '$KEY > result.json
+	--header 'X-RapidAPI-Key: '$KEY > 'result'$YEAR'.json'
+    ((YEAR++))
+done
+echo "Done."
