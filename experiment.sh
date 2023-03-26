e7ff6ee519d8948715a1ce4a41e4365d42d780c0
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-26 12:10:29 -0500

    Add .env + import key for security

--- experiment.sh
+++ experiment.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+source .env
 curl --request GET \
 	--url 'https://mlb-data.p.rapidapi.com/json/named.team_all_season.bam?season='\''2017'\''&all_star_sw='\''N'\''&sort_order=name_asc' \
 	--header 'X-RapidAPI-Host: mlb-data.p.rapidapi.com' \
-	--header 'X-RapidAPI-Key: '
+	--header 'X-RapidAPI-Key: '$KEY
