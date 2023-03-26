f1c06d2d1bfd3ce9a0336d32d2f2bf2e9918892f
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-26 12:10:02 -0500

    Add successfully authenticated GET

---
+++ experiment.sh
@@ -0,0 +1,4 @@
+curl --request GET \
+	--url 'https://mlb-data.p.rapidapi.com/json/named.team_all_season.bam?season='\''2017'\''&all_star_sw='\''N'\''&sort_order=name_asc' \
+	--header 'X-RapidAPI-Host: mlb-data.p.rapidapi.com' \
+	--header 'X-RapidAPI-Key: '
