24c92203ec8059d47701003b959e04203e5ceb56
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-27 17:28:57 -0500

    Add test to create db and populate it

--- experiment.sh
+++ experiment.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 source .env
-YEAR=2017
-while [ $YEAR -le 2019 ]
-do
-curl --request GET \
-	--url 'https://mlb-data.p.rapidapi.com/json/named.team_all_season.bam?season='\'$YEAR\''&all_star_sw='\''N'\''&sort_order=name_asc' \
-	--header 'X-RapidAPI-Host: mlb-data.p.rapidapi.com' \
-	--header 'X-RapidAPI-Key: '$KEY > 'result'$YEAR'.json'
-    ((YEAR++))
-done
-echo "Done."
+
+createdb "test"
+database="test"
+psql -d $database -c "CREATE TABLE IF NOT EXISTS teams(id SERIAL, data TEXT);"
+psql -d $database -c "INSERT INTO teams(data) VALUES ('A'),('B'),('C');"
+
+# YEAR=2017
+# while [ $YEAR -le 2019 ]
+# do
+# curl --request GET \
+# 	--url 'https://mlb-data.p.rapidapi.com/json/named.team_all_season.bam?season='\'$YEAR\''&all_star_sw='\''N'\''&sort_order=name_asc' \
+# 	--header 'X-RapidAPI-Host: mlb-data.p.rapidapi.com' \
+# 	--header 'X-RapidAPI-Key: '$KEY > 'result'$YEAR'.json'
+#     ((YEAR++))
+# done
+# echo "Done."
