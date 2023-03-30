71fbafd2a5597ba27d0cdb7d009fd67a951163cf
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-30 14:39:30 -0500

    Make ENDPOINT an env variable

--- scripts/import.sh
+++ scripts/import.sh
@@ -6,11 +6,11 @@ mkdir ../files/jsons/tickets
 
 PAGE=1
 EPOCH=0
-URL="https://$DOMAIN.zendesk.com/api/v2/incremental/tickets/cursor.json?&start_time=$EPOCH"
+URL="https://$DOMAIN.$ENDPOINT&start_time=$EPOCH"
 
-while [ $PAGE -le 10 ]
+while [ $PAGE -le 1 ]
 do
-    curl "$URL" \
+    curl --silent "$URL" \
     -v -u "$EMAIL/token:$TOKEN" > "../files/jsons/tickets/page$PAGE.json"
     AFTER_URL=$(jq -r '.after_url' "../files/jsons/tickets/page$PAGE.json")
     URL=$AFTER_URL
