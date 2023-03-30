61a7ffa4275610e1d6bbfe2460fb781c19539576
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-30 08:21:04 -0500

    Authenticating succesful

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source ../.env
-echo $TOKEN
+
+PAGE=1
+EPOCH=0
+curl "https://$DOMAIN.zendesk.com/api/v2/incremental/tickets/cursor.json?per_page=10&start_time=$EPOCH" \
+-v -u "$EMAIL/token:$TOKEN"
