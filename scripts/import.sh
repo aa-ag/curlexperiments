707bc339a352a3198175659ee46c77e425cde1cb
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-30 08:47:09 -0500

    Loop working as expected

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 source ../.env
 
+mkdir ../files/jsons
+mkdir ../files/jsons/tickets
+
 PAGE=1
 EPOCH=0
-curl "https://$DOMAIN.zendesk.com/api/v2/incremental/tickets/cursor.json?per_page=10&start_time=$EPOCH" \
--v -u "$EMAIL/token:$TOKEN"
+URL="https://$DOMAIN.zendesk.com/api/v2/incremental/tickets/cursor.json?&start_time=$EPOCH"
+
+while [ $PAGE -le 10 ]
+do
+    curl "$URL" \
+    -v -u "$EMAIL/token:$TOKEN" > "../files/jsons/tickets/page$PAGE.json"
+    AFTER_URL=$(jq -r '.after_url' "../files/jsons/tickets/page$PAGE.json")
+    URL=$AFTER_URL
+	((PAGE++))
+done
+
+echo "Done."
