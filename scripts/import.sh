403711eb9c1aac63fe05386049105cb03f9d47c0
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-02 14:52:51 -0500

    Rename temporary files to temp instead of files

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ../.env
 
-mkdir ../files/jsons
-mkdir ../files/csvs
+mkdir ../temp/jsons
+mkdir ../temp/csvs
 
 PAGENUMBER=1
 EPOCH=0
@@ -11,18 +11,18 @@ URL="https://$DOMAIN.$ENDPOINT&start_time=$EPOCH"
 
 while [ $END_OF_STREAM != true ]
 do
-    curl "$URL" -v -u "$EMAIL/token:$TOKEN" > "../files/jsons/page$PAGENUMBER.json"
-    jq -r '["url","id","external_id","via","created_at","updated_at","type","subject","raw_subject","description","priority","status","recipient","requester_id","submitter_id","assignee_id","organization_id","group_id","collaborator_ids","follower_ids","email_cc_ids","forum_topic_id","problem_id","has_incidents","is_public","due_at","tags","custom_fields","satisfaction_rating","sharing_agreement_ids","custom_status_id","fields","followup_ids","ticket_form_id","brand_id","allow_channelback","allow_attachments","from_messaging_channel","generated_timestamp"], ( .tickets[] | [.url,.id,.external_id,(.via|tostring),.created_at,.updated_at,.type,.subject,.raw_subject,.description,.priority,.status,.recipient,.requester_id,.submitter_id,.assignee_id,.organization_id,.group_id,(.collaborator_ids|tostring),(.follower_ids|tostring),(.email_cc_ids|tostring),.forum_topic_id,.problem_id,.has_incidents,.is_public,.due_atl,(.tags|tostring),(.custom_fields|tostring),(.satisfaction_rating|tostring),(.sharing_agreement_ids|tostring),.custom_status_id,(.fields|tostring),(.followup_ids|tostring),.ticket_form_id,.brand_id,.allow_channelback,.allow_attachments,.from_messaging_channel,.generated_timestamp] ) | @csv' ../files/jsons/page$PAGENUMBER.json > ../files/csvs/page$PAGENUMBER.csv
-    psql -d "raw_db" -c "\COPY t_raw FROM '../files/csvs/page$PAGENUMBER.csv' DELIMITER ',' CSV HEADER;"
-    AFTER_URL=$(jq -r '.after_url' "../files/jsons/page$PAGENUMBER.json")
+    curl "$URL" -v -u "$EMAIL/token:$TOKEN" > "../temp/jsons/page$PAGENUMBER.json"
+    jq -r '["url","id","external_id","via","created_at","updated_at","type","subject","raw_subject","description","priority","status","recipient","requester_id","submitter_id","assignee_id","organization_id","group_id","collaborator_ids","follower_ids","email_cc_ids","forum_topic_id","problem_id","has_incidents","is_public","due_at","tags","custom_fields","satisfaction_rating","sharing_agreement_ids","custom_status_id","fields","followup_ids","ticket_form_id","brand_id","allow_channelback","allow_attachments","from_messaging_channel","generated_timestamp"], ( .tickets[] | [.url,.id,.external_id,(.via|tostring),.created_at,.updated_at,.type,.subject,.raw_subject,.description,.priority,.status,.recipient,.requester_id,.submitter_id,.assignee_id,.organization_id,.group_id,(.collaborator_ids|tostring),(.follower_ids|tostring),(.email_cc_ids|tostring),.forum_topic_id,.problem_id,.has_incidents,.is_public,.due_atl,(.tags|tostring),(.custom_fields|tostring),(.satisfaction_rating|tostring),(.sharing_agreement_ids|tostring),.custom_status_id,(.fields|tostring),(.followup_ids|tostring),.ticket_form_id,.brand_id,.allow_channelback,.allow_attachments,.from_messaging_channel,.generated_timestamp] ) | @csv' ../temp/jsons/page$PAGENUMBER.json > ../temp/csvs/page$PAGENUMBER.csv
+    psql -d "raw_db" -c "\COPY t_raw FROM '../temp/csvs/page$PAGENUMBER.csv' DELIMITER ',' CSV HEADER;"
+    AFTER_URL=$(jq -r '.after_url' "../temp/jsons/page$PAGENUMBER.json")
     URL=$AFTER_URL
-    IS_END_OF_STREAM=$(jq -r '.end_of_stream' "../files/jsons/page$PAGENUMBER.json")
+    IS_END_OF_STREAM=$(jq -r '.end_of_stream' "../temp/jsons/page$PAGENUMBER.json")
     END_OF_STREAM=$IS_END_OF_STREAM
     ((PAGENUMBER++))
 done
 
 # aws s3 cp ... ... -r
-rm -r ../files/csvs
-rm -r ../files/jsons
+rm -r ../temp/csvs
+rm -r ../temp/jsons
 
 echo "Done."
