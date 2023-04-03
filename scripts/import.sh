168b25bdf3977b54f6b990fce11b4286b0db85e8
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-03 18:27:12 -0500

    Add stagin table creation

--- scripts/import.sh
+++ scripts/import.sh
@@ -11,7 +11,7 @@ URL="https://$DOMAIN.$ENDPOINT&start_time=$EPOCH"
 
 while [ $END_OF_STREAM != true ]
 do
-    curl "$URL" -v -u "$EMAIL/token:$TOKEN" > "../temp/jsons/page$PAGENUMBER.json"
+    curl  --rate 10/m "$URL" -v -u "$EMAIL/token:$TOKEN" > "../temp/jsons/page$PAGENUMBER.json"
     jq -r '["url","id","external_id","via","created_at","updated_at","type","subject","raw_subject","description","priority","status","recipient","requester_id","submitter_id","assignee_id","organization_id","group_id","collaborator_ids","follower_ids","email_cc_ids","forum_topic_id","problem_id","has_incidents","is_public","due_at","tags","custom_fields","satisfaction_rating","sharing_agreement_ids","custom_status_id","fields","followup_ids","ticket_form_id","brand_id","allow_channelback","allow_attachments","from_messaging_channel","generated_timestamp"], ( .tickets[] | [.url,.id,.external_id,(.via|tostring),.created_at,.updated_at,.type,.subject,.raw_subject,.description,.priority,.status,.recipient,.requester_id,.submitter_id,.assignee_id,.organization_id,.group_id,(.collaborator_ids|tostring),(.follower_ids|tostring),(.email_cc_ids|tostring),.forum_topic_id,.problem_id,.has_incidents,.is_public,.due_atl,(.tags|tostring),(.custom_fields|tostring),(.satisfaction_rating|tostring),(.sharing_agreement_ids|tostring),.custom_status_id,(.fields|tostring),(.followup_ids|tostring),.ticket_form_id,.brand_id,.allow_channelback,.allow_attachments,.from_messaging_channel,.generated_timestamp] ) | @csv' ../temp/jsons/page$PAGENUMBER.json > ../temp/csvs/page$PAGENUMBER.csv
     psql -d "raw_db" -c "\COPY t_raw FROM '../temp/csvs/page$PAGENUMBER.csv' DELIMITER ',' CSV HEADER;"
     AFTER_URL=$(jq -r '.after_url' "../temp/jsons/page$PAGENUMBER.json")
@@ -22,7 +22,9 @@ do
 done
 
 # aws s3 cp ... ... -r
-rm -r ../temp/csvs
-rm -r ../temp/jsons
+# rm -r ../temp/csvs
+# rm -r ../temp/jsons
+
+psql -d "raw_db" -c "CREATE TABLE t_stagin AS SELECT * FROM t_raw;"
 
 echo "Done."
