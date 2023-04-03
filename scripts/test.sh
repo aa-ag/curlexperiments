e3926a87693713f5582f679f8a99eb47554f1679
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-03 18:44:32 -0500

    Test parsing json from variable

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,4 @@ END_OF_STREAM=false
 URL="https://$DOMAIN.$ENDPOINT&start_time=$EPOCH"
 
 RESPONSE=$(curl "$URL" -v -u "$EMAIL/token:$TOKEN")
-AFTER_URL=$(jq -r '.after_url' <<<"$RESPONSE")
-END_OF_STREAM=$(jq -r '.end_of_stream' <<<"$RESPONSE")
-echo $AFTER_URL
-echo $END_OF_STREAM
+jq -r '.tickets[]' <<<"$RESPONSE"
