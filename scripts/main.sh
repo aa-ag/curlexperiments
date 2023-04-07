0ad830e1259126a8e4816ce94928ca7e8b5d1213
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-07 09:01:19 -0500

    --checklist working as expected

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-dialog --menu "Choose one:" 10 30 3 1 a 2 b 3 c
+dialog --checklist "Choose toppings:" 10 40 3 \
+        1 setup off \
+        2 database off \
+        3 import off
 # sh setup.sh
 # sh db.sh
 # sh import.sh
