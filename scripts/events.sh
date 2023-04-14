15648966dd8143ff16816988674eea9edf62dfb6
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-14 16:17:45 -0500

    Add first stab at jq'ing returned object

--- scripts/events.sh
+++ scripts/events.sh
@@ -8,8 +8,10 @@ URL="https://$DOMAIN.$ENDPOINT&start_time=$EPOCH"
 
 while [ $END_OF_STREAM != true ]
 do
-    curl "$URL" -v -u "$EMAIL/token:$TOKEN" > '../temp/events1.json'
-    END_OF_STREAM=true
+    RESPONSE=$(curl "$URL" -v -u "$EMAIL/token:$TOKEN")
+    jq -r '["id","via","via_reference_id","type","author_id","body","html_body","plain_body","public","attachments","audit_id","created_at","event_type"], ( .ticket_events[] | [.id,(.via|to_string),.via_reference_id,.type,.author_id,.body,.html_body,.plain_body,.public,.attachments,.audit_id,.created_at,.event_type] ) | @csv' <<<"$RESPONSE" > ../temp/csvs/page$PAGENUMBER.csv
+    IS_END_OF_STREAM=true
+    ((PAGENUMBER++))
 done
 
 echo "Done."
