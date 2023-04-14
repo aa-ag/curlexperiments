44b7068eb4636d1236b63ce4e6e17308a3c6b7a0
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-14 15:58:01 -0500

    Insert curl call into loop

--- scripts/events.sh
+++ scripts/events.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 source ../.env
 
+PAGENUMBER=1
 EPOCH=0
+END_OF_STREAM=false
 URL="https://$DOMAIN.$ENDPOINT&start_time=$EPOCH"
-curl "$URL" -v -u "$EMAIL/token:$TOKEN" > 'events1.json'
+
+while [ $END_OF_STREAM != true ]
+do
+    curl "$URL" -v -u "$EMAIL/token:$TOKEN" > '../temp/events1.json'
+    END_OF_STREAM=true
+done
+
+echo "Done."
